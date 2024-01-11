79b3ca2de7c2d04e470b104e319b9a189cca6acc
ANIL KUMBALA <71859977+anilkumbala@users.noreply.github.com>
2024-01-11 21:55:27 +0530

    Update main.tf

--- ops/CloudRunService/dev/main.tf
+++ ops/CloudRunService/dev/main.tf
@@ -35,3 +35,11 @@ resource "google_cloud_run_v2_service" "default" {
     }
   }
 }
+resource "google_cloud_run_service_iam_binding" "default" {
+  location = google_cloud_run_v2_service.default.location
+  service  = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  members = [
+    "allUsers"
+  ]
+}
