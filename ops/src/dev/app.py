0cb7737f0a2d1ce7bd574f0f78fdf3bc55a004c2
ANIL KUMBALA <71859977+anilkumbala@users.noreply.github.com>
2024-01-22 14:41:27 +0530

    Update app.py

--- ops/src/dev/app.py
+++ ops/src/dev/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! form softility devops team'
+    return 'Hello, World! form softility devops team 01'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9090)
