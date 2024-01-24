26f82fa9e34a52ddc515d06c5371a40c5e435c29
Anil Kumbala <HR311@sgpltech.com>
2024-01-24 17:55:33 +0530

    modified k8s tf files

--- ops/Kubernetes/dev/main.tf
+++ ops/Kubernetes/dev/main.tf
@@ -32,21 +32,3 @@ resource "google_container_cluster" "primary" {
   remove_default_node_pool = true
   initial_node_count       = 1
 }
-
-resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name       = "anil-demo-node-pool"
-  location   = "asia-south1"
-  cluster    = google_container_cluster.primary.name
-  node_count = 3
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-medium"
-
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    service_account = google_service_account.default.email
-    oauth_scopes    = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
