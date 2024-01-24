fdc82d6745d7f6648e131bb611fa6044efcdfb5e
Anil Kumbala <HR311@sgpltech.com>
2024-01-24 18:18:23 +0530

    modified k8s tf files

--- ops/Kubernetes/dev/main.tf
+++ ops/Kubernetes/dev/main.tf
@@ -17,11 +17,6 @@ provider "google" {
   project = "excellent-guide-410011"
 }
 
-resource "google_service_account" "default" {
-  account_id   = "service-account-id"
-  display_name = "Service Account"
-}
-
 resource "google_container_cluster" "primary" {
   name     = "anil-demo-gke-cluster"
   location = "asia-south1"
@@ -29,6 +24,6 @@ resource "google_container_cluster" "primary" {
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
-  remove_default_node_pool = true
+  remove_default_node_pool = false
   initial_node_count       = 1
 }
