9c804065fa6bdea540ef7d6f1ec3b72e28a0eaf0
Anil Kumbala <HR311@sgpltech.com>
2024-01-25 15:53:50 +0530

    adding yml files for service and deploymet

--- ops/src/dev/app.py
+++ ops/src/dev/app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    response = make_response('Hello, World! from development team softility')
+    # HTML content with a specific text color
+    html_content = '<p style="color: blue;">Hello, World! from development team softility</p>'
+    
+    response = make_response(html_content)
     
     # Adding minimum required headers
-    response.headers['Content-Type'] = 'text/plain; charset=utf-8'
+    response.headers['Content-Type'] = 'text/html; charset=utf-8'
     response.headers['Server'] = 'Flask'
     
     return response
