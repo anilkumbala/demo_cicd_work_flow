e576fe01c511a17be98d9e209f51d5de7670c367
Anil Kumbala <HR311@sgpltech.com>
2024-01-25 14:00:18 +0530

    adding yml files for service and deploymet

--- ops/src/dev/app.py
+++ ops/src/dev/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! form softility devops team Anil Kumbala'
+    return 'Hello, World! form softility devops team PRASANNAs'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9090)
