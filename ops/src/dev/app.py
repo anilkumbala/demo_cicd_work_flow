fefed542732a891072d38c1b455b6a242e693578
Anil Kumbala <HR311@sgpltech.com>
2024-01-25 15:23:25 +0530

    adding yml files for service and deploymet

--- ops/src/dev/app.py
+++ ops/src/dev/app.py
@@ -1,20 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-# Health check endpoint
-@app.route('/health')
-def health_check():
-    return 'OK', 200
-
-# Your main endpoint
 @app.route('/')
 def hello_world():
-    try:
-        return render_template('index.html')
-    except Exception as e:
-        app.logger.error(f"An error occurred: {e}")
-        return "Internal Server Error", 500
+    return 'Hello, World! form development teamS'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9090)
